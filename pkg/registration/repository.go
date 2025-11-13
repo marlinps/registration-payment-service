6ab682a5e068f03516db8e24f5c36d2713cd409c
Marlin P. S <50097411+marlinps@users.noreply.github.com>
2025-11-13 20:32:57 +0700

    Update repository.go

--- pkg/registration/repository.go
+++ pkg/registration/repository.go
@@ -59,5 +59,6 @@ func (r *RegistrationRepository) GetAll() ([]entities.Registration, error) {
 // }
 
 // func (r *RegistrationRepository) CancelRegistration(registrationID string) error {
+
 // 	return r.db.Where("registration_id = ?", registrationID).Delete(&entities.Registration{}).Error
 // }
